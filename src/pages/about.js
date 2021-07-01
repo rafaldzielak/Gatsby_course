e03d2109b3d0f6113f307819770ca3962a348dc8
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-07-01 18:38:58 +0200

    Pages, routes, navbar, links, layout & styling

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi rerum
+          eveniet eos accusamus vel quam quisquam iusto architecto voluptate
+          recusandae nesciunt, voluptas amet exercitationem illo obcaecati!
+          Earum quos minima voluptate nihil eum veritatis eaque, atque doloribus
+          est doloremque accusamus eius natus tempore, delectus voluptas
+          adipisci at quasi voluptates animi sit.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi rerum
+          eveniet eos accusamus vel quam quisquam iusto architecto voluptate
+          recusandae nesciunt, voluptas amet exercitationem illo obcaecati!
+          Earum quos minima voluptate nihil eum veritatis eaque, atque doloribus
+          est doloremque accusamus eius natus tempore, delectus voluptas
+          adipisci at quasi voluptates animi sit.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi rerum
+          eveniet eos accusamus vel quam quisquam iusto architecto voluptate
+          recusandae nesciunt, voluptas amet exercitationem illo obcaecati!
+          Earum quos minima voluptate nihil eum veritatis eaque, atque doloribus
+          est doloremque accusamus eius natus tempore, delectus voluptas
+          adipisci at quasi voluptates animi sit.
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default About

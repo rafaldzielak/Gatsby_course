a58a303affe2338a5a215a57c3bcde610d02a65e
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-07-02 19:55:27 +0200

    graphQL inside component with useStaticQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,7 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
-export default function Home({ data }) {
-  console.log(data)
-  const { title, description } = data.site.siteMetadata
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -18,21 +16,7 @@ export default function Home({ data }) {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        <p>
-          {title} - {description}
-        </p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`

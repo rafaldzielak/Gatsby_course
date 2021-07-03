ea4257a6b73f2fd94971fae172f76029e48dc914
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2021-07-03 11:44:15 +0200

    Handle images with gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,21 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     "gatsby-transformer-remark",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
